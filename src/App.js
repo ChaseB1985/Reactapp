65f315c507a214ef754eced9c643cc109be094e7
chaseb1985 <chasebarrier85@gmail.com>
2018-10-23 16:15:55 -0500

    updated functions

--- src/App.js
+++ src/App.js
@@ -1,46 +1,62 @@
 import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
+import FriendCard from "../src/components/FriendCard";
+import Wrapper from "../src/components/Wrapper";
+import Title from "../src/components/Title";
+import Counter from "../src/components/Counter/Counter";
+import friends from "../src/friends.json";
 import "./App.css";
 
+
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    friends,
+    count: 0
   };
 
-  // state = {
-  //   count: 0
-  // };
 
-  // handleIncrement increases this.state.count by 1
   handleIncrement = () => {
     // We always use the setState method to update a component's state
     this.setState({ count: this.state.count + 1 });
   };
-
+//select friend function
+//get the id of the friend then this.setstate friends.id=update to true
+handleShuffle = () => {
+  let shuffledFriends = shuffleFriends(friends);
+  this.setState({ friends: shuffledFriends });
+};
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
+      <div>
+      <Counter 
+      count={this.state.count}
+      handleIncrement={this.handleIncrement}
+      />
       <Wrapper>
         <Title>Friends List</Title>
+       
         {this.state.friends.map(friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
-            //count={this.state.count}
-            //handleIncrement={this.handleIncrement}
+            //removeFriend={this."selectfriend"}
+            count={this.state.count}
             id={friend.id}
             key={friend.id}
             name={friend.name}
             image={friend.image}
-            realName={friend.realName}
-            quote={friend.quote}
+            handleShuffle={this.handleShuffle}
           />
         ))}
       </Wrapper>
+      </div>
     );
   }
 }
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,39 +2,21 @@ import React from "react";
 import CardBody from "./CardBody";
 
 // By extending the React.Component class, Counter inherits functionality from it
-class Counter extends React.Component {
-  // Setting the initial state of the Counter component
-  state = {
-    count: 0
-  };
 
-  // handleIncrement increases this.state.count by 1
-  handleIncrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ count: this.state.count + 1 });
-  };
+  const Counter = props => (
 
-  // handleDecrement decreases this.state.count by 1
-  handleDecrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ count: this.state.count - 1 });
-  };
 
-  // The render method returns the JSX that should be rendered
-  render() {
-    return (
       <div className="card text-center">
         <div className="card-header bg-primary text-white">
           Click Counter!
         </div>
-        <CardBody
-          count={this.state.count}
-          handleIncrement={this.handleIncrement}
-          handleDecrement={this.handleDecrement}
-        />
+        <div>
+          <h1>count={props.count}</h1>
+          <button onClick={props.handleIncrement}>Increment</button>
+        </div>
+        
       </div>
     );
-  }
-}
+  
 
 export default Counter;
